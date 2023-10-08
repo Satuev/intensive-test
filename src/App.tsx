a6ddcbd3b3281712cfe0a819136a4cefdbdf9aa4
Satuev <alitm.s@mail.ru>
2023-10-09 02:03:20 +0300

    add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -12,7 +12,7 @@ function App() {
         </button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
